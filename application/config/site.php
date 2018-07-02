649d62fb07fc2b645b12a651449137940e5165a6
yz <yuzheng@51syx.com>
2018-07-02 18:42:51 +0800

    网站配置

--- application/config/site.php
+++ application/config/site.php
@@ -3,8 +3,8 @@
 defined('SYSPATH') OR die('No direct access allowed.');
 
 return array(
-    'website' => 'http://local.myhome.com/',
-    'image' => 'http://local.myhome.com/static/images/',
-    'css' => 'http://local.myhome.com/static/css/',
-    'js' => 'http://local.myhome.com/static/js/',
+    'website' => 'http://yuzhenghome.com/',
+    'image' => 'http://yuzhenghome.com/static/images/',
+    'css' => 'http://yuzhenghome.com/static/css/',
+    'js' => 'http://yuzhenghome.com/static/js/',
 );
